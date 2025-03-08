fbe353021b60a874ff4e21f98b4ded9d6173e32f
R2DWess <gusterlima@hotmail.com>
2025-03-08 04:37:42 -0300

    fix - regra de pesquisa por caracteristicas

--- src/main/java/br/com/wzzy/biblioteca/repository/LivroRepository.java
+++ src/main/java/br/com/wzzy/biblioteca/repository/LivroRepository.java
@@ -4,6 +4,8 @@ import br.com.wzzy.biblioteca.model.BibliotecaModel;
 import br.com.wzzy.biblioteca.model.LivroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LivroRepository extends JpaRepository<LivroModel, Long> {
     LivroModel findByIdLivro(long idLivro);
 
@@ -12,4 +14,9 @@ public interface LivroRepository extends JpaRepository<LivroModel, Long> {
     boolean existsByIdLivro(Long idLivro);
 
     long idLivro(Long idLivro);
+
+    boolean existsByTituloLivro(String tituloLivro);
+
+    Optional<LivroModel> findByTituloLivroAndAutorAndCategoria(String titulo, String categoria, String autor);
+
 }
