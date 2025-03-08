7970bb678064ab439eababb53868340d16ae03bd
R2DWess <gusterlima@hotmail.com>
2025-03-08 15:40:11 -0300

    feat(customers): add custom exceptions for invalid customer data

---
+++ src/main/java/br/com/wzzy/biblioteca/exception/ClienteCadastradoException.java
@@ -0,0 +1,7 @@
+package br.com.wzzy.biblioteca.exception;
+
+public class ClienteCadastradoException extends RuntimeException {
+    public ClienteCadastradoException(String message) {
+        super(message);
+    }
+}
