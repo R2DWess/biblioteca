5ea3eded0440567ba46bae0bc8894f3681acd857
R2DWess <gusterlima@hotmail.com>
2025-03-09 14:48:11 -0300

    feat(books): add book search by id and ID existence check

--- src/main/java/br/com/wzzy/biblioteca/repository/LivroRepository.java
+++ src/main/java/br/com/wzzy/biblioteca/repository/LivroRepository.java
@@ -9,18 +9,5 @@ import java.util.Optional;
 public interface LivroRepository extends JpaRepository<Livro, Long> {
     List<Livro> findByIdLivro(Long idLivro);
 
-    List<Livro> idLivro(Long idLivro);
-//    Livro findByIdLivro(long idLivro);
-//
-//    void deleteByIdLivro(Long idLivro);
-//
-//    boolean existsByIdLivro(Long idLivro);
-//
-//    long idLivro(Long idLivro);
-//
-//    boolean existsByTituloLivro(String tituloLivro);
-//
-//    Optional<Livro> findByTituloLivroAndAutorAndCategoria(String titulo, String categoria, String autor);
-//
-//}
+    void deleteByIdLivro(Long idLivro);
 }
