b010990bbf9a3552ff77234149a70707a3dcddaf
R2DWess <gusterlima@hotmail.com>
2025-03-16 23:56:55 -0300

    refactor: remover dependência do dotenv e usar application.properties

--- src/main/java/br/com/wzzy/biblioteca/BibliotecaApplication.java
+++ src/main/java/br/com/wzzy/biblioteca/BibliotecaApplication.java
@@ -1,17 +1,11 @@
 package br.com.wzzy.biblioteca;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BibliotecaApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
         SpringApplication.run(BibliotecaApplication.class, args);
     }
 }
